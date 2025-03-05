9a85467f0ad24aab22b61d4faa42483e3f52696b
Nirmal Pal <142670318+nrmlpl@users.noreply.github.com>
2025-03-05 11:07:35 +0530

    integrating shadcn and forming signup page

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,21 +1,9 @@
-'use client'
+import React from 'react'
 
-import { useSession, signIn, signOut } from "next-auth/react";
-
-export default function component() {
-  const { data: session } = useSession();
-  if(session) {
-    return (
-      <>
-      Signed in as {session.user.email} <br />
-      <button type="submit" onClick={() => signOut()}>Sign out</button>
-      </>
-    )
-  }
+const page = () => {
   return (
-    <>
-    Not signed in <br />
-    <button type="submit" className="bg-blue-500 rounded-md px-3 py-1 text-white m-4 border-2 border-blue-500" onClick={() => signIn()}>Sign In</button>
-    </>
+    <div>Sign In page</div>
   )
 }
+
+export default page
